ef6f701116be862d75456d039e31406724fde4a6
eduardolagolima <eduardolago95@hotmail.com>
2020-04-13 22:20:23 -0300

    Ajustando title e favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Eduardo Lago Lima',
     meta: [
       { charset: 'utf-8' },
       {

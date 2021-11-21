b5df84e080e80f51ea211e094c1e98bd05f719b1
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-11-20 23:41:59 -0300

    remove data, escola e horas dos cursos

--- components/Tile/TileCourse.vue
+++ components/Tile/TileCourse.vue
@@ -2,26 +2,9 @@
   <article class="tile is-child notification" :class="colorType">
     <p class="title">{{ course.name }}</p>
     <div class="content">
-      <ul>
-        <li>
-          <span>Escola:</span>
-          <strong>{{ course.school }}</strong>
-        </li>
-        <li>
-          <span>Data:</span>
-          <strong>{{ course.date }}</strong>
-        </li>
-        <li v-if="course.hours">
-          <span>Carga horária:</span>
-          <strong>{{ course.hours }} horas</strong>
-        </li>
-        <li v-if="course.urlCertificate">
-          <span>Certificado:</span>
-          <a target="_blank" :href="course.urlCertificate"
-            ><strong>Link</strong></a
-          >
-        </li>
-      </ul>
+      <a target="_blank" :href="course.urlCertificate">
+        <strong>Certificado</strong>
+      </a>
     </div>
   </article>
 </template>

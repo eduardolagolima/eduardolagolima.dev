8f81d0e0bafdd9dc705bdf6230851666c4c9bf4f
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-11-24 18:25:14 -0300

    adiciona componente Button

---
+++ components/Button/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-button
+    v-if="url"
+    target="_blank"
+    :href="url"
+    tag="a"
+    type="is-primary"
+    inverted
+    outlined
+    :icon-right="icon"
+  >
+    {{ label }}
+  </b-button>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: null
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    url: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>

1b253df6a0ae918d6db20620bbe311b235a566f6
Eduardo Lago Lima <eduardolagolima@gmail.com>
2021-12-19 20:08:18 -0300

    adiciona breve descrição aos cursos

--- data/courses.js
+++ data/courses.js
@@ -4,6 +4,8 @@ const courses = [
   {
     icon: 'react',
     title: 'React Native',
+    subtitle:
+      'React Native: Desenvolva APPs Nativas para Android e iOS - Cod3r',
     actions: [
       getCertificateUrl('courses', 'react-native.pdf'),
       getRepositoryUrl('curso-cod3r-react-native'),
@@ -12,6 +14,7 @@ const courses = [
   {
     icon: 'language-typescript',
     title: 'TypeScript',
+    subtitle: 'Dominando TypeScript - Hcode',
     actions: [
       getCertificateUrl('courses', 'typescript.pdf'),
       getRepositoryUrl('curso-hcode-typescript'),
@@ -20,6 +23,7 @@ const courses = [
   {
     icon: 'language-css3',
     title: 'CSS Flexbox',
+    subtitle: 'CSS Flexbox - Origamid',
     actions: [
       getCertificateUrl('courses', 'css-flexbox.pdf'),
       getRepositoryUrl('curso-origamid-cssflexbox'),

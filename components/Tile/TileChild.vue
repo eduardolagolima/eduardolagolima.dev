f6a75085965a4a5bd14f9de7bc678a78a4780080
Eduardo Lago Lima <eduardolagolima@gmail.com>
2023-07-11 15:57:38 -0300

    Refactor/simplifies tiles components (#20)

--- components/Tile/TileChild.vue
+++ components/Tile/TileChild.vue
@@ -1,35 +1,55 @@
 <template>
-  <article :class="['tile', 'is-child', 'notification', type]">
-    <TileChildTitle :title="item.title" :icon="item.icon" />
-    <TileChildSubtitle :subtitle="item.subtitle" />
-    <TileChildContent :actions="item.actions" :tags="item.tags" />
+  <article class="tile is-child notification is-primary">
+    <p class="title">
+      <b-icon v-if="item.icon" :icon="item.icon" size="is-medium" />
+      {{ item.title }}
+    </p>
+
+    <p v-if="item.subtitle" class="subtitle">
+      {{ item.subtitle }}
+    </p>
+
+    <div class="content">
+      <b-taglist v-if="item.tags">
+        <b-tag
+          v-for="(tag, key) of item.tags"
+          :key="key"
+          size="is-small"
+          type="is-primary is-light"
+        >
+          # {{ tag }}
+        </b-tag>
+      </b-taglist>
+
+      <div v-if="item.actions" class="buttons">
+        <ActionButton
+          v-for="(action, key) of item.actions"
+          :key="key"
+          :icon="action.icon"
+          :label="action.label"
+          :url="action.url"
+        />
+      </div>
+    </div>
   </article>
 </template>
 
 <script lang="ts">
 import Vue, { PropOptions } from 'vue'
 
-import TileChildTitle from '~/components/Tile/TileChildTitle.vue'
-import TileChildSubtitle from '~/components/Tile/TileChildSubtitle.vue'
-import TileChildContent from '~/components/Tile/TileChildContent.vue'
+import ActionButton from '~/components/Button/ActionButton.vue'
 
 import { Item } from '~/types/item'
 
 export default Vue.extend({
   components: {
-    TileChildSubtitle,
-    TileChildTitle,
-    TileChildContent,
+    ActionButton,
   },
   props: {
     item: {
       type: Object,
       required: true,
     } as PropOptions<Item>,
-    type: {
-      type: String,
-      required: true,
-    },
   },
 })
 </script>

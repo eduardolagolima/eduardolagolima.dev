043e9ba8707322f359a048fefb3af3ac615acc7a
Eduardo Lago Lima <eduardolagolima@gmail.com>
2023-10-02 22:15:36 -0300

    docs: add `statement-parser` project (#29)

--- data/projects.ts
+++ data/projects.ts
@@ -5,8 +5,15 @@ export const projects: Item[] = [
   {
     icon: 'mdi mdi-36px mdi-account',
     title: 'eduardolagolima.dev',
-    subtitle: 'Este site que você está usando...',
+    subtitle: 'My personal site',
     tags: ['Nuxt.js', 'TypeScript', 'Bulma', 'Netlify'],
     actions: [getRepository('eduardolagolima.dev')],
+  },
+  {
+    icon: 'mdi mdi-36px mdi-clipboard-text',
+    title: 'statement-parser',
+    subtitle: 'CLI para parsear os extratos da Avenue e Clear',
+    tags: ['TypeScript'],
+    actions: [getRepository('statement-parser')],
   },
 ]
